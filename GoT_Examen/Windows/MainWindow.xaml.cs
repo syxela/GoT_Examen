a2ad31b0ac7b923c1adafc28801daaf010034868
syxela <157579434+syxela@users.noreply.github.com>
2024-03-10 19:41:42 +0100

    Toevoegen van een 2de scherm

--- GoT_Examen/Windows/MainWindow.xaml.cs
+++ GoT_Examen/Windows/MainWindow.xaml.cs
@@ -27,7 +27,13 @@ namespace GoT_Examen
                 " to the right words. With each correct answer, you'll earn points and climb closer to the Iron Throne of knowledge." +
                 "\n\n \n\nDo you have what it takes to decipher the words of kings, queens, knights, and schemers? Step into \n\n" +
                 " the Game of Thrones Quote Challenge and prove your mastery of the Seven Kingdoms!";
+        }
 
+        private void btnHomeContinue_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 SecondWindow = new Window2();
+            this.Close();    
+            SecondWindow.Show();
         }
     }
 }
---
+++ GoT_Examen/Windows/Window2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoT_Examen
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+
+            txtChoice.Text = "Below you find the different difficulty levels of this quiz.\n\n"+
+                "Please click on a button to enter the quiz.";
+            
+        }
+    }
+
+}
