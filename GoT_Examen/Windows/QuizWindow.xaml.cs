67af306532559ae0b20a85976e90f05a01155b99
syxela <157579434+syxela@users.noreply.github.com>
2024-03-21 16:50:24 +0100

    Update QuizWindow.xaml.cs

--- GoT_Examen/Windows/QuizWindow.xaml.cs
+++ GoT_Examen/Windows/QuizWindow.xaml.cs
@@ -25,21 +25,38 @@ namespace GoT_Examen.Windows
         private string _buttonClicked;
         private string correctCharacterName; // Variabele om de naam van het correcte karakter bij te houden
         string apiUrl = "https://api.gameofthronesquotes.xyz/v1/random";
+<<<<<<< Updated upstream
         string apiUrl2 = "https://thronesapi.com/api/v2/Characters"; // Tweede API-URL toegevoegd
 
+=======
+        string apiUrl2 = "https://thronesapi.com/api/v2/Characters/";
+>>>>>>> Stashed changes
         public QuizWindow(string buttonClicked)
         {
             InitializeComponent();
             _buttonClicked = buttonClicked;
 
+<<<<<<< Updated upstream
             if (_buttonClicked == "Easy" || _buttonClicked == "Intermediate" || _buttonClicked == "Hard") // Check of de knop is ingedrukt
+=======
+
+            if (_buttonClicked == "Easy")
+            {
+                GetQuote();
+            }
+            else if (_buttonClicked == "Intermediate")
+            {
+                GetQuote();
+            }
+            else if (_buttonClicked == "Hard")
+>>>>>>> Stashed changes
             {
                 GetQuote();
                 GetCharacters(); // Roep de methode op om de afbeeldingen op te halen
             }
         }
 
-        private async void GetQuote()
+        public async void GetQuote()
         {
             using (HttpClient client = new HttpClient())
             {
@@ -51,8 +68,13 @@ namespace GoT_Examen.Windows
 
                     JsonDocument jsonResponse = JsonDocument.Parse(responseBody);
 
+<<<<<<< Updated upstream
                     string quote = jsonResponse.RootElement.GetProperty("sentence").GetString();
                     correctCharacterName = jsonResponse.RootElement.GetProperty("character").GetProperty("name").GetString(); // Sla de naam van het correcte karakter op
+=======
+                string Quote = jsonResponse.RootElement.GetProperty("sentence").GetString();
+                string Author = jsonResponse.RootElement.GetProperty("character").GetProperty("name").GetString();
+>>>>>>> Stashed changes
 
                     // Voeg "Quote: " toe aan de opgehaalde citaatstring
                     quote = "Quote: " + quote;
@@ -76,6 +98,7 @@ namespace GoT_Examen.Windows
         {
             using (HttpClient client = new HttpClient())
             {
+<<<<<<< Updated upstream
                 try
                 {
                     HttpResponseMessage response = await client.GetAsync(apiUrl2);
@@ -107,6 +130,14 @@ namespace GoT_Examen.Windows
                     // Fout bij het deserialiseren van de JSON
                     MessageBox.Show($"Fout bij het deserialiseren van de JSON: {ex.Message}");
                 }
+=======
+                HttpResponseMessage response = await client.GetAsync(apiUrl2);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+
+
+>>>>>>> Stashed changes
             }
         }
 
