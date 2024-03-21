2a167a85a668747b1eb2289703499184486e6566
Mathias <mathias.kerckhof@hotmail.com>
2024-03-21 17:12:11 +0100

    Proberen oproepen image

--- GoT_Examen/Windows/QuizWindow.xaml.cs
+++ GoT_Examen/Windows/QuizWindow.xaml.cs
@@ -1,55 +1,29 @@
-﻿using GoT_Examen.Klassen; // Voeg de namespace van de klassen toe
+﻿using GoT_Examen.Klassen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GoT_Examen.Windows
 {
-    /// <summary>
-    /// Interaction logic for QuizWindow.xaml
-    /// </summary>
     public partial class QuizWindow : Window
     {
         private string _buttonClicked;
         private string correctCharacterName; // Variabele om de naam van het correcte karakter bij te houden
         string apiUrl = "https://api.gameofthronesquotes.xyz/v1/random";
-<<<<<<< Updated upstream
-        string apiUrl2 = "https://thronesapi.com/api/v2/Characters"; // Tweede API-URL toegevoegd
-
-=======
         string apiUrl2 = "https://thronesapi.com/api/v2/Characters/";
->>>>>>> Stashed changes
+
         public QuizWindow(string buttonClicked)
         {
             InitializeComponent();
             _buttonClicked = buttonClicked;
 
-<<<<<<< Updated upstream
             if (_buttonClicked == "Easy" || _buttonClicked == "Intermediate" || _buttonClicked == "Hard") // Check of de knop is ingedrukt
-=======
-
-            if (_buttonClicked == "Easy")
-            {
-                GetQuote();
-            }
-            else if (_buttonClicked == "Intermediate")
-            {
-                GetQuote();
-            }
-            else if (_buttonClicked == "Hard")
->>>>>>> Stashed changes
             {
                 GetQuote();
                 GetCharacters(); // Roep de methode op om de afbeeldingen op te halen
@@ -67,14 +41,8 @@ namespace GoT_Examen.Windows
                     string responseBody = await response.Content.ReadAsStringAsync();
 
                     JsonDocument jsonResponse = JsonDocument.Parse(responseBody);
-
-<<<<<<< Updated upstream
                     string quote = jsonResponse.RootElement.GetProperty("sentence").GetString();
                     correctCharacterName = jsonResponse.RootElement.GetProperty("character").GetProperty("name").GetString(); // Sla de naam van het correcte karakter op
-=======
-                string Quote = jsonResponse.RootElement.GetProperty("sentence").GetString();
-                string Author = jsonResponse.RootElement.GetProperty("character").GetProperty("name").GetString();
->>>>>>> Stashed changes
 
                     // Voeg "Quote: " toe aan de opgehaalde citaatstring
                     quote = "Quote: " + quote;
@@ -98,7 +66,6 @@ namespace GoT_Examen.Windows
         {
             using (HttpClient client = new HttpClient())
             {
-<<<<<<< Updated upstream
                 try
                 {
                     HttpResponseMessage response = await client.GetAsync(apiUrl2);
@@ -108,16 +75,18 @@ namespace GoT_Examen.Windows
                     // Deserialiseer de JSON-response naar een lijst van karakters voor de tweede API
                     List<CharacterImage> characters = JsonSerializer.Deserialize<List<CharacterImage>>(responseBody);
 
-                    // Haal 4 willekeurige karakters op
-                    Random random = new Random();
-                    List<CharacterImage> randomCharacters = characters.OrderBy(c => random.Next()).Take(4).ToList();
+                    // Zoek het juiste karakter op basis van correctCharacterName
+                    CharacterImage correctCharacter = characters.FirstOrDefault(c => c.FullName == correctCharacterName);
 
-                    // Vul de afbeeldingen in
-                    List<Image> imageControls = new List<Image> { imgChar1, imgChar2, imgChar3, imgChar4 };
-                    for (int i = 0; i < 4; i++)
+                   if (correctCharacter != null)
                     {
-                        string imageUrl = randomCharacters[i].ImageUrl;
-                        imageControls[i].Source = new BitmapImage(new Uri(imageUrl));
+                        // Stel de afbeelding in voor een van de Image controls in de XAML
+                        imgChar1.Source = new BitmapImage(new Uri(correctCharacter.ImageUrl));
+                    }
+                    else
+                    {
+                        // Het juiste karakter niet gevonden
+                        MessageBox.Show("Het juiste karakter niet gevonden.");
                     }
                 }
                 catch (HttpRequestException ex)
@@ -130,14 +99,6 @@ namespace GoT_Examen.Windows
                     // Fout bij het deserialiseren van de JSON
                     MessageBox.Show($"Fout bij het deserialiseren van de JSON: {ex.Message}");
                 }
-=======
-                HttpResponseMessage response = await client.GetAsync(apiUrl2);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-
-
->>>>>>> Stashed changes
             }
         }
 
