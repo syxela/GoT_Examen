54b5d97dcb5bb322d5ffcb15c6cb8c499720a195
syxela <157579434+syxela@users.noreply.github.com>
2024-03-22 12:22:58 +0100

    Nu krijg je de juiste character die de quote heeft gezegd in de onze lijst. wel nog niet op een willekeurige plaats, maar telkens op het 1ste afbeelding

--- GoT_Examen/Windows/QuizWindow.xaml.cs
+++ GoT_Examen/Windows/QuizWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-
 namespace GoT_Examen.Windows
 {
     public partial class QuizWindow : Window
@@ -17,36 +16,32 @@ namespace GoT_Examen.Windows
         private string correctCharacterName; // Variabele om de naam van het correcte karakter bij te houden
         string apiUrl = "https://api.gameofthronesquotes.xyz/v1/characters";
         string apiUrl2 = "https://thronesapi.com/api/v2/Characters/";
-
         public QuizWindow(string buttonClicked)
         {
             InitializeComponent();
             _buttonClicked = buttonClicked;
-
             if (_buttonClicked == "Easy" || _buttonClicked == "Intermediate" || _buttonClicked == "Hard") // Check of de knop is ingedrukt
             {
                 GetQuote();
-                GetCharacters(); // Roep de methode op om de afbeeldingen op te halen
+                // Roep de methode op om de afbeeldingen op te halen
             }
         }
-
         public async void GetQuote()
         {
             // Haal de lijst met personages en hun quotes op
             List<Character> characters = await GetCharactersWithQuotes();
-
             // Controleer of er personages zijn opgehaald
             if (characters != null && characters.Any())
             {
                 // Kies een willekeurig personage uit de lijst
                 Random random = new Random();
                 Character randomCharacter = characters[random.Next(characters.Count)];
-
                 // Kies een willekeurige quote uit de quotes van het gekozen personage
                 string randomQuote = randomCharacter.quotes[random.Next(randomCharacter.quotes.Count)];
-
                 // Toon de gekozen quote in de txtQuote
                 txtQuote.Text = "Quote: " + randomQuote;
+                correctCharacterName = randomCharacter.name;
+                GetCharacters(correctCharacterName);
             }
             else
             {
@@ -54,27 +49,27 @@ namespace GoT_Examen.Windows
                 txtQuote.Text = "Er zijn geen personages gevonden.";
             }
         }
-
-        private async void GetCharacters()
+        private async void GetCharacters(string correctChar)
         {
             // Haal de lijst met personages en hun quotes op
             List<Character> characters = await GetCharactersWithQuotes();
-
             if (characters != null && characters.Any())
             {
+                
                 Random random = new Random();
-
                 // Kies willekeurig vier personages
                 List<Character> selectedCharacters = new List<Character>();
-                for (int i = 0; i < 4; i++)
+
+                //Zorgen dat correcte character zeker in de lijst zit 
+                selectedCharacters.Add(characters.Find(c => c.name == correctChar));
+
+                for (int i = 0; i < 3; i++)
                 {
                     Character randomCharacter = characters[random.Next(characters.Count)];
                     selectedCharacters.Add(randomCharacter);
                 }
-
                 // Haal de volledige namen van de personages op
                 List<string> fullNames = await GetCharacterFullNames(selectedCharacters);
-
                 // Maak een lijst met CharacterImage-objecten
                 List<CharacterImage> characterImages = new List<CharacterImage>();
                 foreach (var fullName in fullNames)
@@ -84,11 +79,11 @@ namespace GoT_Examen.Windows
                 }
 
                 // Wijs de afbeeldingen toe aan de Image-elementen
-                    imgChar1.Source = new BitmapImage(new Uri(characterImages[0].imageUrl));
-                    imgChar2.Source = new BitmapImage(new Uri(characterImages[1].imageUrl));
-                    imgChar3.Source = new BitmapImage(new Uri(characterImages[2].imageUrl));
-                    imgChar4.Source = new BitmapImage(new Uri(characterImages[3].imageUrl));
-             
+                imgChar1.Source = new BitmapImage(new Uri(characterImages[0].imageUrl));
+                imgChar2.Source = new BitmapImage(new Uri(characterImages[1].imageUrl));
+                imgChar3.Source = new BitmapImage(new Uri(characterImages[2].imageUrl));
+                imgChar4.Source = new BitmapImage(new Uri(characterImages[3].imageUrl));
+
 
             }
         }
@@ -96,7 +91,6 @@ namespace GoT_Examen.Windows
         private async Task<List<string>> GetCharacterFullNames(List<Character> characters)
         {
             List<string> fullNames = new List<string>();
-
             using (HttpClient client = new HttpClient())
             {
                 try
@@ -104,23 +98,24 @@ namespace GoT_Examen.Windows
                     HttpResponseMessage response = await client.GetAsync(apiUrl2);
                     response.EnsureSuccessStatusCode();
                     string responseBody = await response.Content.ReadAsStringAsync();
-
                     // Deserialiseer de JSON-response naar een lijst van characters
                     List<CharacterImage> characterImages = JsonSerializer.Deserialize<List<CharacterImage>>(responseBody);
 
                     // Zoek de volledige naam voor elk personage uit de lijst van characters
-                    while(fullNames.Count < 4) 
-                    { 
+                    while (fullNames.Count < 4)
+                    {
                         foreach (var character in characters)
+                        {
+                            CharacterImage characterImage = characterImages.FirstOrDefault(c => c.fullName == character.name);
+                            if (characterImage != null)
                             {
-                                CharacterImage characterImage = characterImages.FirstOrDefault(c => c.fullName == character.name);
-                                if (characterImage != null)
-                                {
-                                    fullNames.Add(characterImage.fullName);
-                                }
+                                fullNames.Add(characterImage.fullName);
                             }
                         }
                     }
+                }
+
+
                 catch (HttpRequestException ex)
                 {
                     // Fout bij het maken van de HTTP-request
@@ -132,7 +127,6 @@ namespace GoT_Examen.Windows
                     MessageBox.Show($"Fout bij het deserialiseren van de JSON: {ex.Message}");
                 }
             }
-
             return fullNames;
         }
 
@@ -140,7 +134,6 @@ namespace GoT_Examen.Windows
         private async Task<string> GetCharacterImageUrl(string characterName)
         {
             string imageUrl = string.Empty;
-
             using (HttpClient client = new HttpClient())
             {
                 try
@@ -148,13 +141,10 @@ namespace GoT_Examen.Windows
                     HttpResponseMessage response = await client.GetAsync(apiUrl2);
                     response.EnsureSuccessStatusCode();
                     string responseBody = await response.Content.ReadAsStringAsync();
-
                     // Deserialiseer de JSON-response naar een lijst van characters
                     List<CharacterImage> characterImages = JsonSerializer.Deserialize<List<CharacterImage>>(responseBody);
-
                     // Zoek de juiste afbeelding op basis van de naam van het personage
                     var characterImage = characterImages.FirstOrDefault(c => c.fullName == characterName);
-
                     if (characterImage != null)
                     {
                         imageUrl = characterImage.imageUrl;
@@ -171,15 +161,11 @@ namespace GoT_Examen.Windows
                     MessageBox.Show($"Fout bij het deserialiseren van de JSON: {ex.Message}");
                 }
             }
-
             return imageUrl;
         }
-
-
         public async Task<List<Character>> GetCharactersWithQuotes()
         {
             List<Character> characters = new List<Character>();
-
             using (HttpClient client = new HttpClient())
             {
                 try
@@ -187,7 +173,6 @@ namespace GoT_Examen.Windows
                     HttpResponseMessage response = await client.GetAsync(apiUrl);
                     response.EnsureSuccessStatusCode();
                     string responseBody = await response.Content.ReadAsStringAsync();
-
                     // Deserialiseer de JSON-response naar een lijst van characters
                     characters = JsonSerializer.Deserialize<List<Character>>(responseBody);
                 }
@@ -202,7 +187,6 @@ namespace GoT_Examen.Windows
                     MessageBox.Show($"Fout bij het deserialiseren van de JSON: {ex.Message}");
                 }
             }
-
             return characters;
         }
     }
