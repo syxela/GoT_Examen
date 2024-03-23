f22e7d4bcd53fbfa543e0cf93881156e2a9f292c
syxela <157579434+syxela@users.noreply.github.com>
2024-03-23 17:12:00 +0100

    Toevoegen Ex

---
+++ Ex/Klassen/Scoreboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex.Klassen
+{
+    public class Scoreboard
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public int Score {  get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
